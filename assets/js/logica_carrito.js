006af70fd60ee42d9dd43cd62c44a514e0726ce0
Nelson Ramirez <xhyskoax@gmail.com>
2025-12-03 18:00:05 -0300

    se agregan eventos al DOM que interactuan con el carrito

--- assets/js/logica_carrito.js
+++ assets/js/logica_carrito.js
@@ -1,6 +1,7 @@
 const carrito = {
     productos: [],
     delivery: 3_990,
+    descuentos: 0,
     buscarProductos: function(id){
         let productoBuscado = this.productos.find(
             (producto) => producto.id == id
@@ -45,12 +46,10 @@ const carrito = {
         );
 
         if (indiceElemento >= 0) {
-            console.log("elemento encontrado.");
-
-            this.productos.splice(indiceElemento, 1);
-            return this.productos;
+            let productosEliminados = this.productos.splice(indiceElemento, 1);
+            return productosEliminados;
         } else {
-            console.log("Producto no encontrado en el carrito");
+            return false;
         }
     },
     cantidadProductos: function () {
@@ -60,15 +59,8 @@ const carrito = {
         if (this.productos.length == 0) {
             return alert("Carrito vació.");
         }
-
-        let confirmacion = confirm(
-            "¿Está seguro que desea eliminar todos los productos del carrito?"
-        );
-
-        if (confirmacion) {
             this.productos.length = 0;
             return alert("Carrito en cero!");
-        }
     },
     productosConDetalle: function(){
 
@@ -82,9 +74,45 @@ const carrito = {
 
         return listaProductosConDetalle;
 
+    },
+    subtotal: function(){
+        let productos = this.productosConDetalle();
+
+        let subtotal = 0;
+
+        productos.forEach(producto => {
+            subtotal+= producto.precio * producto.cantidad;
+        })
+
+        return subtotal;
+    },
+
+    totalConImpuestos: function(){
+        let total = this.subtotal() + this.totalDelivery();
+        return total;
+    },
+    totalDelivery: function(){
+        if(this.productos.length > 0){
+            return this.delivery
+        }else{
+            return 0;
+        }
     }
 };
 
+
+
+function eliminarProducto(id, nombre){
+        let productosEliminados = carrito.eliminarProducto(id);
+        console.log(productosEliminados);
+        let producto = productosEliminados[0];
+        if(producto){
+            alert(`Producto con ID: ${producto.id} (${nombre}) eliminado correctamente.`);
+            main();
+        }else{
+            alert("Este producto ya no se encuentra en su carrito.");
+        }
+};
 
 
 document.addEventListener("DOMContentLoaded", (event) => { 
@@ -120,14 +148,19 @@ document.addEventListener("DOMContentLoaded", (event) => {
                                                     </div>
                                                 </div>
                                                 <div class="d-flex flex-row align-items-center">
-                                                    <div style="width: 50px;">
-                                                        <h5 class="fw-normal mb-0">${producto.cantidad}</h5>
+                                                    <div>
+                                                        <input type="number" min="1" max="10" step="1" value="${producto.cantidad}">
+
                                                     </div>
                                                     <div style="width: 80px;">
                                                         <h5 class="mb-0">$${producto.precio}</h5>
                                                     </div>
-                                                    <a href="#!" style="color: #cecece;"><i
-                                                            class="fas fa-trash-alt"></i></a>
+                                                    <button type="button" class="btn btn-secondary" onclick="eliminarProducto(${producto.id}, '${producto.nombre}')">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"></path>
+                                                        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"></path>
+                                                        </svg>
+                                                    </button>
                                                 </div>
                                             </div>
                                         </div>
@@ -141,11 +174,26 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     function cargarDetalleCobro(carrito){
 
-        document.getElementById("costo-envio").innerText = carrito.delivery;
+        document.getElementById("costo-envio").innerText = carrito.totalDelivery().toLocaleString("es-CL");
+        document.getElementById("subtotal").innerText = carrito.subtotal().toLocaleString("es-CL");
+        document.getElementById("total-con-impuestos").innerText = carrito.totalConImpuestos().toLocaleString("es-CL");
+        document.getElementById("btn-pago").innerText = carrito.totalConImpuestos().toLocaleString("es-CL");
     }
 
 
-    function main(){
+    document.getElementById("btn-vaciar-carro").addEventListener("click", (event)=> {
+        
+        let confirmacion = confirm("¿Está seguro que desea eliminar todos los productos del carrito?");
+
+        if(confirmacion){
+            carrito.limpiarCarrito();
+            main();
+        }
+    });
+
+
+
+    window.main = () => {
 
         let productos = carrito.productosConDetalle();
         agregarTarjetasProductos(productos);
